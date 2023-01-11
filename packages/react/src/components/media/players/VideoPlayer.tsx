a2a76c89ab76fbaa85ad6f38e5197374c706cedb
Chase Adams <c@cadams.io>
2023-01-11 15:04:04 -0700

    fix: add metrics to videoplayer

--- packages/react/src/components/media/players/VideoPlayer.tsx
+++ packages/react/src/components/media/players/VideoPlayer.tsx
@@ -1,11 +1,14 @@
 import { VideoPlayerProps } from '@livepeer/core-react/components';
 import { MediaControllerState } from 'livepeer';
-import { canPlayMediaNatively } from 'livepeer/media/browser';
+import {
+  addMediaMetricsToInitializedStore,
+  canPlayMediaNatively,
+} from 'livepeer/media/browser';
 import { styling } from 'livepeer/media/browser/styling';
 import * as React from 'react';
 import { createPortal } from 'react-dom';
 
-import { useMediaController } from '../../../context';
+import { MediaControllerContext, useMediaController } from '../../../context';
 import { PosterSource } from '../Player';
 
 const mediaControllerSelector = ({
@@ -19,54 +22,84 @@ export type { VideoPlayerProps };
 export const VideoPlayer = React.forwardRef<
   HTMLVideoElement,
   VideoPlayerProps<HTMLVideoElement, PosterSource>
->(({ src, autoPlay, title, loop, muted, poster, objectFit, priority }, ref) => {
-  const { fullscreen } = useMediaController(mediaControllerSelector);
+>(
+  (
+    {
+      src,
+      autoPlay,
+      title,
+      loop,
+      muted,
+      poster,
+      objectFit,
+      priority,
+      onMetricsError,
+    },
+    ref,
+  ) => {
+    const { fullscreen } = useMediaController(mediaControllerSelector);
 
-  const filteredSources = React.useMemo(
-    () => src?.filter((s) => s?.mime && canPlayMediaNatively(s)),
-    [src],
-  );
+    const filteredSources = React.useMemo(
+      () => src?.filter((s) => s?.mime && canPlayMediaNatively(s)),
+      [src],
+    );
 
-  return (
-    <video
-      className={styling.media.video({
-        size: fullscreen ? 'fullscreen' : objectFit,
-      })}
-      loop={loop}
-      aria-label={title ?? 'Video player'}
-      role="video"
-      autoPlay={autoPlay}
-      width="100%"
-      height="100%"
-      ref={ref}
-      webkit-playsinline="true"
-      playsInline
-      muted={muted}
-      poster={typeof poster === 'string' ? poster : undefined}
-      preload={priority ? 'auto' : 'metadata'}
-    >
-      {typeof document !== 'undefined' &&
-        createPortal(
-          <>
-            {filteredSources?.[0]?.mime && (
-              <link
-                rel="preload"
-                as="video"
-                href={filteredSources[0].src}
-                type={filteredSources[0].mime}
-              />
-            )}
-          </>,
-          document.head,
-        )}
-      {filteredSources?.map((source) => (
-        <source key={source.src} src={source.src} type={source.mime!} />
-      ))}
-      {
-        "Your browser doesn't support the HTML5 <code>video</code> tag, or the video format."
-      }
-    </video>
-  );
-});
+    const store = React.useContext(MediaControllerContext);
+
+    React.useEffect(() => {
+      const { destroy } = addMediaMetricsToInitializedStore(
+        store,
+        filteredSources?.[0]?.src,
+        (e) => {
+          onMetricsError?.(e as Error);
+          console.error('Not able to report player metrics', e);
+        },
+      );
+
+      return destroy;
+    }, [onMetricsError, store, filteredSources]);
+
+    return (
+      <video
+        className={styling.media.video({
+          size: fullscreen ? 'fullscreen' : objectFit,
+        })}
+        loop={loop}
+        aria-label={title ?? 'Video player'}
+        role="video"
+        autoPlay={autoPlay}
+        width="100%"
+        height="100%"
+        ref={ref}
+        webkit-playsinline="true"
+        playsInline
+        muted={muted}
+        poster={typeof poster === 'string' ? poster : undefined}
+        preload={priority ? 'auto' : 'metadata'}
+      >
+        {typeof document !== 'undefined' &&
+          createPortal(
+            <>
+              {filteredSources?.[0]?.mime && (
+                <link
+                  rel="preload"
+                  as="video"
+                  href={filteredSources[0].src}
+                  type={filteredSources[0].mime}
+                />
+              )}
+            </>,
+            document.head,
+          )}
+        {filteredSources?.map((source) => (
+          <source key={source.src} src={source.src} type={source.mime!} />
+        ))}
+        {
+          "Your browser doesn't support the HTML5 <code>video</code> tag, or the video format."
+        }
+      </video>
+    );
+  },
+);
 
 VideoPlayer.displayName = 'VideoPlayer';
